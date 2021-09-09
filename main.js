7b5266e5635aa6257232cc9d41f5c59816039351
sarthakmisra <78964203+sarthakmisra@users.noreply.github.com>
2021-09-09 21:45:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+fallarray = []
+function searching() {
+    var mane = document.getElementById("s1").value;
+
+    fallarray.push(mane);
+    document.getElementById("sow").style.display="block";
+}
